7b5fbb728e855f8d2374aade15576e085adfe39d
Hana-kuragechan <k023g1049@g.neec.ac.jp>
2024-07-18 15:26:33 +0900

    07_02確認課題完了

--- main.cpp
+++ main.cpp
@@ -1,7 +1,31 @@
 #include<stdio.h>
-
+#include<thread>
+#include <iostream>
+void Quadruple(int num) {
+	num *= 4;
+	std::cout << "thread id=" << std::this_thread::get_id() << std::endl;
+	std::cout << "num*4=" << num << std::endl;
+}
+void AddFour(int num) {
+	num += 4;
+	std::cout << "thread id=" << std::this_thread::get_id() << std::endl;
+	std::cout << "num+4=" << num << std::endl;
+}
+void SubtractFour(int num) {
+	num -= 4;
+	std::cout << "thread id=" << std::this_thread::get_id() << std::endl;
+	std::cout << "num-4=" << num << std::endl;
+}
 int main() {
+	int num = 5;
 
+	std::thread thread1(Quadruple, num);
+	thread1.join();
 
+	std::thread thread2(AddFour, num);
+	thread2.join();
+
+	std::thread thread3(SubtractFour, num);
+	thread3.join();
 	return 0;
 }
